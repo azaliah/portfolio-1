7a5d60d804a9f85e3b16e07789962b5896a3b907
Abhinav prasad <Abhinavprasad791@gmail.com>
2020-08-18 22:00:55 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import django_heroku
 
 import os
 
@@ -123,3 +124,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+django_heroku.settings(locals())
